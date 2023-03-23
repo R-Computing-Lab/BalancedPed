a2bc197d4616e53e65ef0d71d1807775f0eaccb2
Xuanyu Lyu <lxy750909935@gmail.com>
2023-03-23 11:57:07 -0400

    back up before commenting lines

--- PowerPlot.R
+++ PowerPlot.R
@@ -1,22 +1,25 @@
 ### this is a script to generate power plots for the simulation results
 
-
-## Run all the scripts
-for (p in 1: 11){
-      for (c in 1: 11){
-            dir <- paste0("~/R-Project/BalancedPed/Simulations/p",p,"/p",p, "c",c)
-            if (dir.exists(dir)){
-                  cat("starttime:", as.character(Sys.time())) 
-                  cat("running", dir)
-                  setwd(dir)
-                  source(paste0("p",p,"c",c,".R"))
-                  cat(dir, "finished at" , as.character(Sys.time()))
-            }else{
-                  next
+run_protected_loop <- function(){
+      for (p in 1: 11){
+            for (c in 1: 11){
+                  dir <- paste0("~/R-Project/BalancedPed/Simulations/p",p,"/p",p, "c",c)
+                  if (dir.exists(dir)){
+                        cat("starttime:", as.character(Sys.time())) 
+                        cat("running", dir)
+                        setwd(dir)
+                        source(paste0("p",p,"c",c,".R"))
+                        cat("finished at" , as.character(Sys.time()))
+                  }else{
+                        next
+                  }
             }
+            
       }
-      
 }
+## Run all the scripts
+run_protected_loop()
+
 
 ## Generate power plots for all the conditions
 
